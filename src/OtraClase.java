a6169923781f8318fc882507ad1030b2f41f82be
Geny Castañeda <genny21cas@gmail.com>
2023-10-02 19:42:10 -0500

    agregando otra clase

---
+++ src/OtraClase.java
@@ -0,0 +1,15 @@
+public class OtraClase {
+
+
+    int a;
+    int b;
+
+    public void multiplicar(int a, int b)
+    {
+        a= 5;
+        b=24;
+        int result = a*b;
+        System.out.println(result);
+
+    }
+}
